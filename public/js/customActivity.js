2c0c26a1c9066c5fac9303d8bbdbcb7cdef6c3cc
aress-exness <78411334+aress-exness@users.noreply.github.com>
2021-05-26 15:17:14 +0530

    Shashi

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -114,7 +114,7 @@ define([
           "headers": {
             "Content-Type": "application/json"
           },
-          "data": JSON.stringify({"to":"e726da50548bb85ecc63d0","data":{"title":"Hello World Custom Activity","body":"Hello World!"},"notification":{"body":"Hello World ✌"}}),
+          "data": JSON.stringify({"to":"e726da50548bb85ecc63d0","data":{"title":"Hello World Shashi","body":"Hello World!"},"notification":{"body":"Hello World ✌"}}),
         };
 
         $.ajax(settings).done(function (response) {
